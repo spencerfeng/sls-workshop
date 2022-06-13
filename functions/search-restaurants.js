31cfdf05a023ee323654108cfab6ef1aeae2dbd9
Spencer Feng <spencer.fengicloud.com>
2022-06-13 20:21:05 +1000

    initial commit

---
+++ functions/search-restaurants.js
@@ -0,0 +1,29 @@
+const DocumentClient = require("aws-sdk/clients/dynamodb").DocumentClient;
+const dynamodb = new DocumentClient();
+
+const defaultResults = process.env.defaultResults || 8;
+const tableName = process.env.restaurants_table;
+
+const findRestaurantsByTheme = async (theme, count) => {
+    const req = {
+        TableName: tableName,
+        Limit: count,
+        FilterExpression: "contains(themes, :theme)",
+        ExpressionAttributeValues: { ":theme": theme },
+    };
+
+    const resp = await dynamodb.scan(req).promise();
+    return resp.Items;
+};
+
+module.exports.handler = async (event, context) => {
+    const req = JSON.parse(event.body);
+    const theme = req.theme;
+    const restaurants = await findRestaurantsByTheme(theme, defaultResults);
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(restaurants),
+    };
+
+    return response;
+};
